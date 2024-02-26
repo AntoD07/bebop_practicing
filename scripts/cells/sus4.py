63004eb225e1657cc3df557852aab1e8a697e878
Antony Doukhan <antonydoukhan@chlsmacbook05.home>
2024-02-26 23:42:24 +0100

    Added dominant 3rd and 5th cells

--- scripts/cells/sus4.py
+++ scripts/cells/sus4.py
@@ -4,7 +4,20 @@ dominant_essential_starting_cells = {
         "Coltrane Pattern from root": ["1", "2", "3", "5", "7"],
         "Lee Morgan Dominant": ["1", "maj7", "7", "9", "6"],
         "Minor Honeysuckle Rose": ["1", "7", "2", "4", "6"],
-    }
+    },
+    3: {
+        "Dominant Bebop Scale from 3rd": ["3", "9", "1", "maj7", "7"],
+        "Dominant 9th Arpeggio": ["3", "5", "7", "9", "1"],
+        "Dexter Gordon": ["3", "4", "9", "7", "6"],
+        "Descending Dominant Scale": ["3", "9", "1", "7", "6"],
+    },
+    5: {
+        "Ascending II Scale": ["5", "6", "7", "1", "9"],
+        "Descending Dominant Scale from 5th": ["5", "4", "3", "2", "1"],
+        "Ascending II-Minor 7th Arpeggio": ["5", "7", "9", "4", "3"],
+        "Diatonic Enclosure": ["5", "9", "4", "9", "3"],
+        "Cannonball Adderley Approach": ["5", "4", "3", "4", "2"],
+    },
 }
 
 dominant_essential_ending_cells = {
@@ -13,5 +26,18 @@ dominant_essential_ending_cells = {
         "Freddie Hubbard": ["9", "4", "3", "9", "1"],
         "Dominant Turn": ["6", "5", "#11", "5", "1"],
         "Dominant 9th Arpeggio": ["3", "5", "7", "9", "1"],
-    }
+    },
+    3: {
+        "Descending Minor Scale from 3th (7rd on dominant)": ["7", "6", "5", "4", "3"],
+        "Coltrane Pattern from 7th": ["7", "1", "9", "4", "3"],
+        "Diatonic Enclosure": ["5", "9", "4", "9", "3"],
+        "Passing Tone Enclosure": ["9", "4", "9", "#9", "3"],
+        "Ascending II-Minor 7th Arpeggio": ["5", "7", "9", "4", "3"],
+    },
+    5: {
+        "Dominant Bebop scale from root": ["1", "maj7", "7", "6", "5"],
+        "Stan Getz enclosure of II Root": ["7", "4", "#11", "6", "5"],
+        "Sonny Stitt Enclosure of II Root": ["#11", "6", "#5", "#11", "5"],
+        "Johnny Griffin Enclosure": ["9", "4", "#11", "6", "5"],
+    },
 }
